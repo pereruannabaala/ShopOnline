c528e92d2c7b3dc1dcd48d468f3a5905ef8e10a8
harry <harrisongitau3@gmail.com>
2024-01-12 03:11:47 +0300

    Dev: Update flask templates and link to relevant routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,42 @@
 from app import app
+from flask import render_template, url_for, flash, redirect, request
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello world'
+    return render_template('index.html', title='Index')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html', title='Shop')
+
+
+@app.route('/account')
+def account():
+    return render_template('auth/login.html', title='Login')
+
+
+# ---------------------
+# Start: Auth
+# ---------------------
+
+
+@app.route('/request-password-reset')
+def request_password_reset():
+    return render_template('auth/request_password_reset.html', title='Request Password Reset')
+
+
+@app.route('/reset-password')
+def reset_password():
+    return render_template('auth/reset_password.html', title='Reset Password')
+
+# ---------------------
+# Start: Auth
+# ---------------------
